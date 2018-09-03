289e9cdc133264c5d39df2903049ff22f71ce238
hirokisuganuma <da78245@qa3.so-net.ne.jp>
2018-09-03 08:20:56 +0000

    herokuS3setting

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
       # Amazon S3用の設定
       :provider              => 'AWS',
       :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AKIAJI4OPPKLYOTDR37A'],
-      :aws_secret_access_key => ENV['lnSThCU7mWXaB0LH1pXlY1z / UAX + bCgwr1pIDXlI']
+      :aws_access_key_id     => ENV['AKIAJE5HX3ZTK6NYEIFA'],
+      :aws_secret_access_key => ENV['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']
     }
     config.fog_directory     =  ENV['rails-tutorial071137']
   end
