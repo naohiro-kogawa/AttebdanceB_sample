65ba68a9533075c902c4704172edcf4578847418
hirokisuganuma <da78245@qa3.so-net.ne.jp>
2018-09-18 04:09:59 +0000

    swichtja

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,7 +4,7 @@ class MicropostsController < ApplicationController
  def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created!"
+      flash[:success] = "投稿が完了しました。"
       redirect_to root_url
     else
       @feed_items = []
@@ -13,7 +13,7 @@ class MicropostsController < ApplicationController
  end
  def destroy
     @micropost.destroy
-    flash[:success] = "Micropost deleted"
+    flash[:success] = "投稿を削除しました。"
     redirect_to request.referrer || root_url
  end
 
