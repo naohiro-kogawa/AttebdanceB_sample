4df26704c24c271660c1c9a06b50ef97fb6adf5b
hirokisuganuma <da78245@qa3.so-net.ne.jp>
2018-09-26 01:39:33 +0000

    s3setteng2

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,10 +2,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   
-     storage :file
-  
-
   # アップロードファイルの保存先ディレクトリは上書き可能
   # 下記はデフォルトの保存先  
   def store_dir
@@ -13,7 +15,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
 
   # アップロード可能な拡張子のリスト
-  def extension_whitelist
+  def extension_white_list
     %w(jpg jpeg gif png)
   end
 end
