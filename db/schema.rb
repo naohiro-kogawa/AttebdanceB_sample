6b8ffcefba1642e35118f437f4e6632e94ca816b
EC2 Default User <ec2-user@ip-172-31-34-162.ap-southeast-1.compute.internal>
2018-10-26 08:12:51 +0000

    初めのコミット

--- db/schema.rb
+++ db/schema.rb
@@ -19,10 +19,13 @@ ActiveRecord::Schema.define(version: 20181022061315) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "micropost_id"
+    t.integer "user_id", null: false
+    t.integer "micropost_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["micropost_id"], name: "index_likes_on_micropost_id"
+    t.index ["user_id", "micropost_id"], name: "index_likes_on_user_id_and_micropost_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "microposts", force: :cascade do |t|
