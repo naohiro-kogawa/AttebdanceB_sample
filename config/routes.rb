4111a7350921a72c69d6631208eb589aa0df0bfe
EC2 Default User <ec2-user@ip-172-31-34-162.ap-southeast-1.compute.internal>
2018-10-27 08:46:57 +0000

    ルートファイル修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,25 @@
 Rails.application.routes.draw do
   
-  get 'practice/index'
-  post 'practice/index'
-get  '/test',  to: 'practice#index'
   root                'static_pages#home'
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
-  get    'hoges'    => 'static_pages#test'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-  get 'static_pages/test'
-  post 'static_pages/test'
   resources :users do
-    resources :apples
-    member do
-      get :following, :followers
-    end
+   # member do
+    #  get :following, :followers
+   # end
   end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   #resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
-  resources :likes, only: [:create, :destroy]
+ # resources :likes, only: [:create, :destroy]
   #like機能拡張用に指定
-  resources :microposts do
-    resources :likes, only: [:create, :destroy]
-  end
+ # resources :microposts do
+ #   resources :likes, only: [:create, :destroy]
+ # end
 end
