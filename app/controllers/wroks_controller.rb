74ae507fad38316fa5fe0b05ad7c094ad3dca2c6
EC2 Default User <ec2-user@ip-172-31-34-162.ap-southeast-1.compute.internal>
2018-10-27 09:34:00 +0000

    show見やすく変更

--- app/controllers/wroks_controller.rb
+++ app/controllers/wroks_controller.rb
@@ -1,2 +1,9 @@
 class WroksController < ApplicationController
+    def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "投稿が完了しました。"
+      redirect_to root_url
+  end
+  end
 end
