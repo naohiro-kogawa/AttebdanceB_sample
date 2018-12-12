3d0ed2226dce9ef94ad40a0df991432f3a44eb1d
shotaimai66 <changemymind6@gmail.com>
2018-12-12 21:37:16 +0000

    fixed: works_helperに一日の勤務時間のロジックを移動し、showページを簡単にしました。

--- app/helpers/wroks_helper.rb
+++ app/helpers/wroks_helper.rb
@@ -1,2 +1,19 @@
 module WroksHelper
+    # このmoduleではworkが定義されてないので、メソッドに引数を渡してあげる(work)。このメソッドをビューに書くだけです。下のメソッドの処理もコピペしただけです。
+    def day_work_time(work)
+        work.leaving_time && work.attendance_time && sprintf("%.2f", BigDecimal(((work.leaving_time - work.attendance_time)/60/60).to_s).to_f )
+    end
+    
+    # 以下は一月の計算方法です。
+    # def month_work_time(work, year, month)これをビューへ記入。yearとmonthは下のdaysを算出するのに使用する。
+    #     total = 0
+    #     days = １ヶ月分の日付のオブジェクト
+    #     days.each do |day|
+    #         if work = Work.find_by(day: day)
+    #             total = total + work.leaving_time - work.attendance_time
+    #         end
+    #     end
+    #     totalをsprintfなどを使って調整。
+    # end
+
 end
